50793c793d0a96b335612d8f83bd259d36a9fc58
Prtish Behera <pritishbehera125@gmail.com>
2026-03-06 23:03:26 +0530

    Added review delete route

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const methodOverride = require("method-override");
 const ejsMate= require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const expressError = require("./utils/expressError.js");
-const {listingSchema} = require("./schema.js");
+const {listingSchema,reviewSchema} = require("./schema.js");
 const Review = require("./models/review.js");
 
 app.set("view engine","ejs");
@@ -37,6 +37,18 @@ app.get("/",(req,res)=>{
 
 const validateListing = (req,res,next) => {
     let {error} = listingSchema.validate(req.body);
+    
+    if(error){
+        let errMsg = error.details.map((el)=>el.message).join(",");
+        throw new expressError(400,errMsg);
+    }else{
+        next();
+    }
+    
+};
+
+const validateReview = (req,res,next) => {
+    let {error} = reviewSchema.validate(req.body);
     
     if(error){
         let errMsg = error.details.map((el)=>el.message).join(",");
@@ -98,7 +110,7 @@ app.put("/listings/:id", wrapAsync(async(req,res)=>{
 //show route
 app.get("/listings/:id",wrapAsync(async (req,res)=>{
     let {id}= req.params;
-    let list= await Listing.findById(id);
+    let list= await Listing.findById(id).populate("reviews");
     res.render("./listings/show.ejs",{list});
 }));
 
@@ -110,22 +122,34 @@ app.delete("/listings/:id",wrapAsync(async(req,res)=>{
     res.redirect("/listings");
 }));
 
-app.post("/listings/:id/reviews",async(req,res)=>{
+//review post route
+app.post("/listings/:id/reviews",validateReview,async(req,res)=>{
+    let {id} = req.params;
     let listing = await Listing.findById(req.params.id);
     let newReview = new Review(req.body.review);
     listing.reviews.push(newReview);
     await newReview.save();
     await listing.save();
-    console.log("new review saved");
-    res.send("your reviews got saved");
-    
+    // console.log("new review saved");
+    // res.send("your reviews got saved");
+    res.redirect(`/listings/${id}`);
 });
+
+//review delete Route
+app.delete("/listings/:id/reviews/:reviewId",wrapAsync(async(req,res)=>{
+    let {id,reviewId} = req.params;
+    await Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}});
+    await Review.findByIdAndDelete(reviewId);
+
+    res.redirect(`/listings/${id}`);
+}));
 
 app.use((err,req,res,next)=>{
     let{statusCode=500,message="something went wrong"}=err;
     res.status(statusCode).render("error.ejs",{message});
     // res.status(statusCode).send(message);
 });
+
 app.use((req,res,next) => {
     if(!res.headersSent){
     next( new expressError(404,"Page Not Found"));
